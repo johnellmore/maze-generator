f2f3f6dc8bf11920d10e4c7126531a7acefcba0f
John Ellmore <jnellmore@gmail.com>
2021-06-04 16:15:50 -0500

    first algorithm

--- algorithms.js
+++ algorithms.js
@@ -4,3 +4,30 @@ export function* randomGenerator(maze) {
         yield [b];
     }
 }
+
+export function* randomDepthFirstSearchGenerator(maze) {
+    maze.graph.setAllBoundaries(true);
+    const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+    const allCells = maze.graph.cells();
+    const startCell = pickRandom(allCells);
+
+    const backtrackStack = [startCell];
+    const visitedIds = new Set([startCell.id]);
+    while (backtrackStack.length) {
+        // find all unvisited neighbors
+        const thisCell = backtrackStack.pop();
+        yield [thisCell];
+        const unvisitedNeighbors = thisCell.neighbors
+            .filter(cell => !visitedIds.has(cell.id));
+        if (unvisitedNeighbors.length === 0) {
+            continue;
+        }
+        backtrackStack.push(thisCell);
+        const nextCell = pickRandom(unvisitedNeighbors);
+        visitedIds.add(nextCell.id);
+        backtrackStack.push(nextCell);
+        const wallToRemove = thisCell.boundaryBetween(nextCell);
+        wallToRemove.isWall = false;
+        yield [wallToRemove];
+    }
+}
